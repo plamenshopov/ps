3b040f440ce4009d0ed15d606a2ecd0abb21de00
plamenshopov <plamenshopov@gmail.com>
2023-02-13 14:35:55 +0200

    test

---
+++ function Test-RemotePC.ps1
@@ -0,0 +1,22 @@
+function Test-RemotePC {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$true)][string]$PCname
+    )
+    Invoke-Command -ComputerName $PCname -ScriptBlock {
+    begin{
+        Write-Output ""
+        Write-Output "$($PSStyle.bold)Beginning the check$($PSStyle.bold)"
+        
+    }
+
+    process {
+        get-process | Where-Object name -like Veeam.Guest.Interaction.Proxy
+    }
+
+    end{
+        Write-Output ""
+        Write-Host "$($PSStyle.bold)The check is completed$($PSStyle.bold)"
+    }
+    }
+}
