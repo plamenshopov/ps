e3e3cb6568f9ae90da486199320945760b5780fd
Plamen Shopov <plamenshopov@gmail.com>
2023-02-14 17:43:36 +0200

    new version

--- try.ps1
+++ try.ps1
@@ -2,23 +2,23 @@ try {
     $ErrorActionPreference = "Stop"
 
     Invoke -ComputerName $PCname -ScriptBlock {
-    begin{
-        Write-Output ""
-        Write-Output "$($PSStyle.bold)Beginning the check$($PSStyle.bold)"
-        
-    }
+        begin {
+            Write-Output ""
+            Write-Output "$($PSStyle.bold)Beginning the check$($PSStyle.bold)"
+        }
 
-    process {
-        get-process | Where-Object name -like Veeam.Guest.Interaction.Proxy
-    }
+        process {
+            get-process | Where-Object name -like Veeam.Guest.Interaction.Proxy
+        }
 
-    end{
-        Write-Output ""
-        Write-Host "$($PSStyle.bold)The check is completed$($PSStyle.bold)"
-    }
+        end {
+            Write-Output ""
+            Write-Host "$($PSStyle.bold)The check is completed$($PSStyle.bold)"
+        }
     }
 } 
-    catch {
-        $ErrorMessage = $_.Exception.Message
-        Write-Host $ErrorMessage
-        exit(1)}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage
+    exit(1)
+}
